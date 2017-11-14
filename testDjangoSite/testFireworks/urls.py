b7f9979a5e81705b11dd02c606bf1a8462428889
Ueda-Ichitaka <ueda@debCastle>
2017-11-14 20:16:31 +0100

    added simple fireworks task execution to views

--- testDjangoSite/testFireworks/urls.py
+++ testDjangoSite/testFireworks/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-
-from . import views
-from .views import IndexView, EditorView
+from django.views.generic.base import RedirectView
+from testFireworks.views import IndexView, EditorView, TaskerRedirectView
 
 app_name = 'testFireworks'
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^editor/', EditorView.as_view(), name='editor'),
+    url(r'^editor/$', EditorView.as_view(), name='editor'),
+    url(r'^tasker/$', TaskerRedirectView.as_view(url='http://heise.de'), name='tasker'),
+    #url(r'^tasker/$', TaskerRedirectView.as_view(url='http://127.0.0.1:5000'), name='tasker'),
 ]
--- testDjangoSite/testFireworks/views.py
+++ testDjangoSite/testFireworks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-
+from django.views.generic.base import TemplateView, RedirectView
+from fireworks import Firework, LaunchPad, ScriptTask, FWorker
+from fireworks.core.rocket_launcher import rapidfire
+#from fireworks.scripts.lpad_run import webgui
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -9,4 +11,31 @@ class IndexView(TemplateView):
     
 class EditorView(TemplateView):
     template_name = "testFireworks/editor.html" 
+    
+class TaskerRedirectView(RedirectView):    
+    
+    permanent = True
+    query_string = False
+    pattern_name = 'webgui'
+    
+    def get_redirect_url(self, *args, **kwargs):
+        
+        # set up the LaunchPad and reset it
+        launchpad = LaunchPad(host="127.0.0.1", port=27017, name="fw_tutorial", username="fwtestadmin", password="bla")
+        launchpad.reset('', require_password=False)
+
+        # create the Firework consisting of a single task
+        ft1 = ScriptTask.from_str('echo "howdy, your job launched successfully!"')
+        ft2 = ScriptTask.from_str('echo "hello world"')
+        ft3 = ScriptTask.from_str('echo "where am i?"')
+        firework = Firework([ft1, ft2, ft3])
+
+        # store workflow and launch it locally
+        launchpad.add_wf(firework)
+        rapidfire(launchpad, FWorker())
+        #launchpad.
+        
+        return super(TaskerRedirectView, self).get_redirect_url(*args, **kwargs)
+    
+    
     
