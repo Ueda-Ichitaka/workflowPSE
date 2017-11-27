3270949d95bfbb5abba97a0f1e200a5ac4be9fd9
Ueda-Ichitaka <ueda.ichitaka@gmx.de>
2017-11-27 16:56:12 +0100

    removed unused folder stubs; moved django test project to code folder; created and populated folder for Pflichtenheft

--- install pse.sh
+++ install pse.sh
@@ -22,30 +22,30 @@ sudo make install
 sudo pip3 install Django
 
 ## install mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 ## debian 8 wheezy
 ## for debian 7 view mongodb install page
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/testing main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt-get update
-sudo apt-get install -f -y mongodb-org
+#echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/testing main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+#sudo apt-get update
+#sudo apt-get install -f -y mongodb-org
 
 # start mongodb
-sudo service mongod start
+#sudo service mongod start
 
 ## install fireworks
-sudo pip3 install FireWorks
-sudo pip3 install matplotlib # for visual report plots in web gui
-sudo pip3 install paramiko   # for built in remote file transfer
-sudo pip3 install fabric     # for daemon mode of qlaunch
-sudo pip3 install requests   # for NEWT queue adapter
+#sudo pip3 install FireWorks
+#sudo pip3 install matplotlib # for visual report plots in web gui
+#sudo pip3 install paramiko   # for built in remote file transfer
+#sudo pip3 install fabric     # for daemon mode of qlaunch
+#sudo pip3 install requests   # for NEWT queue adapter
 
-cd /home/$user/Desktop
-echo "host: ds049170.mongolab.com
-port: 49170
-name: fireworks
-username: test_user
-password: testing123" | tee my_launchpad_testing.yaml
-lpad -l my_launchpad_testing.yaml get_wflows
+#cd /home/$user/Desktop
+#echo "host: ds049170.mongolab.com
+#port: 49170
+#name: fireworks
+#username: test_user
+#password: testing123" | tee my_launchpad_testing.yaml
+#lpad -l my_launchpad_testing.yaml get_wflows
 
 
 
