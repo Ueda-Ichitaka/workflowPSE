e8117520ae97a8a3d0ad06b59bb97e228dd4208e
Denis Gensh <denis.gensh@gmail.com>
2018-01-09 22:32:20 +0100

    Modified settings.py module. Added lists used by cron

--- code/server/base/cron.py
+++ code/server/base/cron.py
@@ -10,11 +10,14 @@ Django crontab. Version, die bei mir sicher funktioniert hat
 
 
 def first_crontab_task():
-    os.mkdir('/home/paradigmen/C/' + str(random.randrange(1, 100)) + '/')
+    #os.mkdir('/home/paradigmen/C/' + str(random.randrange(1, 100)) + '/')
+    #Testzeile
+    pass
 
 
 """
 Django cron. Das geht bei mir immer noch nicht :(
+Wenn ich richtig verstanden habe, dann muss man den Manage Befehl (also python3 manage.py runcrons) selbst in Cron eintragen.
 """
 
 
@@ -25,5 +28,6 @@ class FirstCronTask(CronJobBase):
     code = 'testCron.firstCronTask'
 
     def do(self):
-        os.mkdir('/home/paradigmen/C/' + str(random.randrange(1, 100)) + '/')
-        
+        #os.mkdir('/home/paradigmen/C/' + str(random.randrange(1, 100)) + '/')
+        #Testzeile
+        pass
