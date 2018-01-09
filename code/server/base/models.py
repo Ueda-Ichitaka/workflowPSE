beeadd4247c4893b5bd7ee97821b0bba99a0b920
Ueda-Ichitaka <ueda@debCastle>
2018-01-09 13:05:53 +0100

    begin models; fix base view

--- code/server/base/models.py
+++ code/server/base/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Session(models.Model):
     user = models.ForeignKey(User, editable= False)
-    last_workflow = models.ForeignKey(Workflow)
+    #last_workflow = models.ForeignKey(Workflow)
     
     #def __str__(self):
      #   return self.type_value
