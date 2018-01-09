327143eee65a8a0273af67d4b4e160d2166e1981
Ueda-Ichitaka <ueda.ichitaka@gmx.de>
2018-01-09 15:45:43 +0100

    fully coded models from design document

--- code/server/base/models.py
+++ code/server/base/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 from django.contrib.auth.models import User
-from unittest.util import _MAX_LENGTH
 
 # Create your models here.
 
@@ -32,8 +30,8 @@ class Workflow(models.Model):
     percent_done = models.DecimalField(max_digits=3, decimal_places=0)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
     updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
-    creator = models.ForeignKey(User, editable=False)
-    last_modifier = models.ForeignKey(User, editable=True)
+    creator = models.ForeignKey(User, editable=False, related_name='creator_user', on_delete=models.CASCADE)
+    last_modifier = models.ForeignKey(User, editable=True, null=True, blank=True, on_delete=models.SET_NULL)
     
 
 class WPSProvider(models.Model):
@@ -60,8 +58,8 @@ class Process(models.Model):
         
 
 class Task(models.Model):    
-    workflow = models.ForeignKey(Workflow)
-    process = models.ForeignKey(Process)
+    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
     x = models.DecimalField(max_digits=5, decimal_places=0)
     y = models.DecimalField(max_digits=5, decimal_places=0)
     status = models.CharField(max_length=1, choices=STATUS)
@@ -69,20 +67,37 @@ class Task(models.Model):
     abstract = models.TextField('Descriptive text', default='Add your super descriptive text here...')
     status_url = models.URLField(max_length=1000)
     started_at = models.DateTimeField(auto_now=False, auto_now_add=False)
-    
-
 
 
 class Session(models.Model):
-    user = models.ForeignKey(User, editable= False)
-    last_workflow = models.ForeignKey(Workflow)
-    
+    user = models.ForeignKey(User, editable= False, on_delete=models.CASCADE)
+    last_workflow = models.ForeignKey(Workflow, null=True, on_delete=models.SET_NULL)
+
+
+class InputOutput(models.Model):
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, choices=ROLE)
+    identifier = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    abstract = models.TextField('Descriptive text', default='Add your super descriptive text here...')
+    datatype = models.CharField(max_length=1, choices=DATATYPE)
+    min_occurs = models.IntegerField()
+    max_occurs = models.IntegerField()
+
 
 class Edge(models.Model):
-
-
+    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    from_task = models.ForeignKey(Task, related_name='out_task', on_delete=models.CASCADE) #rename to out_task?
+    to_task = models.ForeignKey(Task, on_delete=models.CASCADE)  #rename to in_task?
+    input = models.ForeignKey(InputOutput, related_name='input', null=True, on_delete=models.SET_NULL)
+    output =models.ForeignKey(InputOutput, null=True, on_delete=models.SET_NULL)
     
     
-class InputOutput(models.Model):
-    
-class Artefact(models.Model):            
+class Artefact(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    parameter = models.ForeignKey(InputOutput, null=True, on_delete=models.SET_NULL)
+    role = models.CharField(max_length=1, choices=ROLE)
+    format = models.CharField(max_length=200)
+    data = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
