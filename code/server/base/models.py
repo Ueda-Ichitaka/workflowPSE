5ab3b4c7a87897b5b50d614e726108674fc4a141
Ueda-Ichitaka <ueda@debCastle>
2018-01-09 12:45:18 +0100

    start add models

--- code/server/base/models.py
+++ code/server/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Session(models.Model):
+    user = models.ForeignKey(User, editable= False)
+    last_workflow = models.ForeignKey(Workflow)
+    
+    #def __str__(self):
+     #   return self.type_value
