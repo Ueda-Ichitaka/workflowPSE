9d5f7989fea59653d5c6b10e2f5c8692041797a0
Ueda-Ichitaka <ueda.ichitaka@gmx.de>
2018-01-23 20:30:17 +0100

    added working xml generation for tasks; not finished

--- code/server/base/cron.py
+++ code/server/base/cron.py
@@ -1,8 +1,9 @@
-import os
+import os, sys
 import random
 import xml.etree.ElementTree as ET
 from base.models import WPSProvider, WPS, Task, InputOutput, Artefact, Process
 from email import policy
+from xml.dom import minidom
 
 #from django_cron import Schedule, CronJobBase
 
@@ -20,51 +21,84 @@ def first_crontab_task():
 
 def scheduler():
     # Scheduler main function
-    # check workflow list for execute flag
-    # for all tasks in db do
-    #    check status for execute status
     
-    os.mkdir('/home/ueda/Desktop/test/')
+    # redirect stout to file
+    #orig_stdout  = sys.stdout
+    #f = open('/home/ueda/workspace/PSE/code/server/outfile.txt', 'w')
+    #sys.stdout = f
     
-    
-    with open('outfile.txt', 'a') as the_file:
-        the_file.write('Hello\n')
     
     task_list = list(Task.objects.filter(status='0').values())
     for task in task_list:        
-        print(task["id"], task["process_id"], task["title"], task["status"], sep=" ")
-        task_id = task["id"]   # id of task
-        wf_id=task["workflow_id"]  # id of workflow of task
-        proc_id=task["process_id"]  # id of pywps process, evaluate to identifier
+        #print("")
+        #print("<wps:Execute>")     
+        #print("task id:", task["id"], "process id:", task["process_id"], "task titel:", task["title"], "task status:", task["status"], sep=" ")
+ 
+        root = ET.Element('wps:Execute')
+        root.set('service', 'WPS')
+        root.set('version', '1.0.0')
+ 
+        #task_id = task["id"]   # id of task
+        #wf_id=task["workflow_id"]  # id of workflow of task
+        #proc_id=task["process_id"]  # id of pywps process, evaluate to identifier
 
         process_list = list(Process.objects.filter(id=task["process_id"]).values())
-        for process in process_list:
-            print(process["id"], process["identifier"], sep=" ")
-
-
+        for process in process_list:            
+            #print("process id:", process["id"], "process identifier:", process["identifier"], sep=" ")
+            
+            identifier = ET.SubElement(root, 'ows:Identifier')
+            identifier.text = process["identifier"]
+            
+        inputs = ET.SubElement(root, 'wps:DataInputs')    
+            
         input_list = list(InputOutput.objects.filter(process_id=task["process_id"], role='0').values())
-        for input in input_list:
-            print(input["id"], input["identifier"], input["title"], input["datatype"], input["format"], sep=" ")
-            input_identifier=input["identifier"]
-            input_title=input["title"]
-            input_datatype=input["datatype"]
-            input_data_format=input["format"]
-
+        for input in input_list:            
+            #print("<input>")
+            #print("input id:", input["id"], "input identifier:", input["identifier"], "input titel:", input["title"], "datatype:", input["datatype"], "format:", input["format"], sep=" ")
+            
+            inputElement = ET.SubElement(inputs, 'wps:Input')
+            inputIdent = ET.SubElement(inputElement, 'ows:Identifier')
+            inputTitle = ET.SubElement(inputElement, 'ows:Title')
+            inputData = ET.SubElement(inputElement, 'wps:Data')
+            inputIdent.text = input["identifier"]
+            inputTitle.text = input["title"]
+                        
+            #input_identifier=input["identifier"]
+            #input_title=input["title"]
+            #input_datatype=input["datatype"]
+            #input_data_format=input["format"]
 
             artefact_list = list(Artefact.objects.filter(task_id=task["id"], parameter=input["id"]).values())
             for artefact in artefact_list:
-                print(artefact["id"], artefact["data"], sep=" ")
-                artefact_data=artefact["data"]
+                
+                data = ET.SubElement(inputData, 'wps:DataTypeComesHere')
+                data.text = artefact["data"]
+                
+                #print("artefact id:", artefact["id"], "data:", artefact["data"], sep=" ")
+                #artefact_data=artefact["data"]
+            
+            #print("</input>")
+        
+        #print("")
+        #print('/home/ueda/workspace/PSE/code/server/base/testfiles/task' + str(task["id"]) + '.xml')
+        
+        #mydata_rough = ET.tostring(root, 'utf-8')
+        #reparsed = minidom.parseString(mydata_rough)
+        #myfile = open('/home/ueda/workspace/PSE/code/server/base/testfiles/task' + str(task["id"]) + '.xml', 'w')
+        #mydata = reparsed.toprettyxml(indent="\t")
+        #myfile.write(str(mydata))
+        
+        tree = ET.ElementTree(root)
+        tree.write('/home/ueda/workspace/PSE/code/server/base/testfiles/task' + str(task["id"]) + '.xml')
+        
+        #print(" ", str(ET.tostring(tree))
+        #myfile.close()
+        
+        
+        
+    #sys.stdout = orig_stdout
+    #f.close()
 
-
-    #    for all tasks to execute do
-    #       traverse InputOutput table
-    #           if InputOutput.process_id == Task.id
-    #               select
-    #       generate process xml
-    #       send xml to wps server
-    #
-    #todo: max parallel tasks schedule policy
 
 
 def scheduler_execute():
@@ -151,7 +185,7 @@ def checkFiles():
 def get_capabilities_parsing():
     #Works only with absolute path.
     #In future will work with url
-    url_from_scc_vm = '/home/denis/Projects/Python/Django/workflowPSE/code/server/base/testfiles/getCapabilitiesFromPyWPS.xml'
+    url_from_scc_vm = '/home/ueda/workspace/PSE/code/server/base/testfiles/getCapabilitiesFromPyWPS.xml'
 
     xml_namespaces = {
         'gml': 'http://www.opengis.net/gml',
@@ -175,7 +209,7 @@ def get_capabilities_parsing():
 
 def parse_service_provider_info(root, namespaces):
     service_provider_element = root.find('ows:ServiceProvider', namespaces)
-    os.mkdir('/home/denis/Documents/' + str(random.randrange(1, 100)) + '/')
+    #os.mkdir('/home/denis/Documents/' + str(random.randrange(1, 100)) + '/')
     provider_name = service_provider_element.find('ows:ProviderName', namespaces).text
     #provider_site = service_provider_element.find('ows:ProviderSite', namespaces).attrib.get(
     #    '{' + namespaces.get('xlink') + '}href')
