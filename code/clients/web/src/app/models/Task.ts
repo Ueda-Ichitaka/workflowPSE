f9f13afad12a8e18bf7d1d5bf952f57a38c7d96d
Marcel Herm <uddtg@student.kit.edu>
2018-01-23 16:58:27 +0100

    added documentation for models and pages

--- code/clients/web/src/app/models/Task.ts
+++ code/clients/web/src/app/models/Task.ts
@@ -1,5 +1,8 @@
 import { Artefact } from 'app/models/Artefact';
 
+/**
+ * describes the current state of a task
+ */
 export enum TaskState {
     NONE,
     READY,
@@ -10,7 +13,12 @@ export enum TaskState {
     DEPRECATED
 }
 
-
+/**
+ * tasks describe the actual task elements which are
+ * displayed in the editor
+ * with process_id, every task links to a process
+ * which is executed if this task is run
+ */
 export interface Task {
     id: number;
     x: number;
@@ -22,3 +30,4 @@ export interface Task {
     created_at: number;
     updated_at: number;
 }
+
--- code/clients/web/src/app/pages/workflows-page/workflows-page.component.ts
+++ code/clients/web/src/app/pages/workflows-page/workflows-page.component.ts
@@ -18,6 +18,12 @@ export class WorkflowsPageComponent implements OnInit {
 
   public openedWorkflowID = -1;
 
+  /**
+   * creates the workflow page object
+   * @param processService fetches process data
+   * @param workflowService fetches workflow data
+   * @param router enables routing
+   */
   constructor(
     private processService: ProcessService,
     private workflowService: WorkflowService,
@@ -26,21 +32,37 @@ export class WorkflowsPageComponent implements OnInit {
 
   }
 
+  /**
+   * is called after an appropriate time to get
+   * all workflows and processes
+   */
   public ngOnInit() {
     this.workflowService.all().subscribe(workflows => this.workflows = workflows);
     this.processService.all().subscribe(processes => this.processes = processes);
   }
 
+  /**
+   * checks wether a workflow is opened
+   * @param workflow the workflow which is checked
+   */
   public opened(workflow: Workflow) {
     this.openedWorkflowID = workflow.id;
   }
 
+  /**
+   * checks wether a workflow is closed
+   * @param workflow the workflow which is checked
+   */
   public closed(workflow: Workflow) {
     if (this.openedWorkflowID === workflow.id) {
       this.openedWorkflowID = -1;
     }
   }
 
+  /**
+   * removes a workflow
+   * @param id the id of the workflow which is removed
+   */
   public remove(id: number) {
     const index = this.workflows.findIndex(workflow => workflow.id === id);
     if (index !== -1) {
@@ -49,22 +71,42 @@ export class WorkflowsPageComponent implements OnInit {
     }
   }
 
+  /**
+   * routes to the editor opening a workflow
+   * @param id the id of the workflow which is opened
+   */
   public edit(id: number) {
     this.router.navigate(['/editor', { id }]);
   }
 
+  /**
+   * gets a workflow from the database
+   * @param id the id of the workflow which is returned
+   */
   public getWorkflow(id: number): Observable<Workflow> {
     return this.workflowService.get(id);
   }
 
+  /**
+   * executes a workflow
+   * @param id the id of the workflow which is executed
+   */
   public run(id: number) {
     this.workflowService.execute(id);
   }
 
+  /**
+   * validates a workflow
+   * @param workflow the id of the workflow which is validated
+   */
   public validate(workflow): boolean {
     return this.workflowService.validate(workflow) === WorkflowValidationResult.SUCCESSFUL;
   }
 
+  /**
+   * checks if a workflow is running
+   * @param worflow the workflow which is checked
+   */
   public runs(worflow: Workflow): boolean {
     return this.workflowService.isRunning(worflow);
   }
